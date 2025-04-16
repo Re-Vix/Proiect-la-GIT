90b37e9da19ac7900da14ab0896ea63eb4a2c3fe
petr0zaurus <gologanpetru@gmail.com>
2025-04-16 04:27:39 -0700

    sfarsitul zilei 15.04.2024

--- app/(tabs)/main.tsx
+++ app/(tabs)/main.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Alert, TouchableOpacity, Image, TextInput } from 'react-native'
+import { View, Text, Alert, TouchableOpacity, Image, TextInput, ScrollView } from 'react-native'
 import { Avatars } from 'react-native-appwrite'
 import { client } from '@/backend/appwrite'
 import { useEffect, useState } from 'react'
@@ -6,6 +6,7 @@ import { Link } from 'expo-router'
 import { account } from '@/backend/appwrite'
 import { Ionicons } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
+import { getQueryAndVariables } from '@/backend/useAnilistAPI'
 
 const main = () => {
 
@@ -34,6 +35,40 @@ useEffect(() => {
     const handleSearch = () => {
       navigation.navigate('explore', { searchQuery });
     };
+
+      const [mangas, setMangas] = useState<any>([])
+      const [hasNextPage, setHasNextPage] = useState(true)
+    
+      const getMangasFromAPI = async () => {
+        const { query, variables } = await getQueryAndVariables(1, 10);
+        const fetchData = async () => {
+          try {
+            const response = await fetch('https://graphql.anilist.co', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+              },
+              body: JSON.stringify({
+                query: query,
+                variables: variables
+              })
+            }).then(response => response.json());
+    
+            setMangas(response.data.Page.media);
+            setHasNextPage(response.data.Page.pageInfo.hasNextPage)
+    
+          } catch (error) {
+            console.log(error);
+          }
+        }
+    
+        fetchData();
+      }
+    
+      useEffect(() => {
+        getMangasFromAPI()
+      }, [])
  
   return (
     <View>
@@ -46,13 +81,23 @@ useEffect(() => {
         </View>
         <Ionicons name='settings-outline' size={30}/>
       </View>
-      <View className='flex-row items-center gap-2 border border-gray-300 rounded-lg px-4 py-3'>
+      <View className='flex-row items-center gap-2 border border-gray-300 rounded-lg px-4 py-3 mt-4 w-[80%] mx-auto'>
+        <TouchableOpacity onPress={handleSearch}>
           <Ionicons name='search-outline' size={24} color='black' />
-          <TextInput value={searchQuery} onChangeText={setSearchQuery} placeholder='Search...' className='flex-1 ml-2 text-gray-700' />
-          <TouchableOpacity onPress={handleSearch} className='bg-blue-500'>
-            <Text>Search</Text>
-          </TouchableOpacity>
+        </TouchableOpacity>
+        <TextInput placeholder='Search...' className='flex-1 ml-2 text-gray-700' onChangeText={setSearchQuery} value={searchQuery} />
         </View>
+        <Text className='text-[20px]'>Trending Mangas</Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {
+        mangas.map((manga: any, index: number) => (
+          <View key={index} className='mt-2'>
+            <Text>{manga.title.english}</Text>
+            <Image className='w-[200px] h-[200px]' source={{uri: manga.coverImage.extraLarge}}/>
+          </View>
+        ))
+      }
+        </ScrollView>
     </View>
   )
 }
